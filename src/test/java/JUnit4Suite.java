2384874c9c54e223a5c725f0406ec7fb228e5540
prajwal <prajwalsr10@gmail.com>
2025-07-03 17:50:06 +0530

    new changes

---
+++ src/test/java/JUnit4Suite.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4Suite.java to edit this template
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Administrator
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestAccount.class, TestCalculator.class})
+public class JUnit4Suite {
+
+}
